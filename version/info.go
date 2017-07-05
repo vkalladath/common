42325f442594871c1c8df79e942933bf804550b0
svsuren <root@tst-ccp-grf-w2-cp-818.corporate.t-mobile.com>
2017-07-05 14:44:12 -0700

    update link to library

--- version/info.go
+++ version/info.go
@@ -20,7 +20,7 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/vkalladath/client_golang/prometheus"
 )
 
 // Build information. Populated at build-time.
